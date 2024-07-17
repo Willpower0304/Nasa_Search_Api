af9ba8fa993228a6e70b7abeca5173fff6283224
William Espinoza <espinozaw657@gmail.com>
2024-07-16 19:21:35 -0500

    feat: Mission Page

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let apiKey = "s5G2pihNdE0wiUkORlzDzNkvZcg65yhgRAuqaAN8";
-let apiUrl = `https://api.nasa.gov/planetary/apod?api_key=s5G2pihNdE0wiUkORlzDzNkvZcg65yhgRAuqaAN8`; 
+window.addEventListener('load', obtenerDatos);
 
-let getInfo = () => {
-    fetch(apiUrl)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-};
+function obtenerDatos() {
+    const apiKey = "s5G2pihNdE0wiUkORlzDzNkvZcg65yhgRAuqaAN8";
+    const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+    const fechas = ['2023-07-01', '2023-07-02', '2023-07-03', '2023-07-04'];
 
-window.addEventListener("load", getInfo);
+    fechas.forEach((fecha, index) => {
+        const apiUrl = `${baseUrl}&date=${fecha}`;
+        fetch(apiUrl)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarDatos(resultado, index + 1));
+    });
+}
+
+function mostrarDatos({ explanation, title, url }, index) {
+    const titulo = document.querySelector(`#titulo${index}`);
+    titulo.innerHTML = title;
+
+    const descripcion = document.querySelector(`#descripcion${index}`);
+    const primerPunto = explanation.indexOf('.');
+    if (primerPunto !== -1) {
+        explanation = explanation.substring(0, primerPunto + 1);
+    }
+    descripcion.innerHTML = explanation;
+
+    const multimedia = document.querySelector(`#imagen${index}`);
+    multimedia.innerHTML = `<img src="${url}" alt="${title}" class="imagen">`;
+}
